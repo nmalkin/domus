6d1d21ebc74b6e14e5d881a7ca9d3a7bee914ce2
Nathan Malkin <nmalkin@cs.brown.edu>
2011-04-24 18:06:12 -0400

    constants file

---
+++ src/Constants.java
@@ -0,0 +1,18 @@
+import java.awt.Color;
+
+
+public class Constants {
+	// Canvas
+	protected static final int CANVAS_WIDTH  = 600;
+	protected static final int CANVAS_HEIGHT = 400;
+	protected static final java.awt.Color CANVAS_COLOR = Color.WHITE;
+
+	// Person images & dimensions
+	protected static final String MAN_FILE   = "/home/nmalkin/course/cs032/domus/img/man.png";
+	protected static final int    MAN_WIDTH  = 37; // pixels
+	protected static final int    MAN_HEIGHT = 100; // pixels
+
+	protected static final String WOMAN_FILE   = "/home/nmalkin/course/cs032/domus/img/woman.png";
+	protected static final int    WOMAN_WIDTH  = 46; // pixels
+	protected static final int    WOMAN_HEIGHT = 100; // pixels
+}
