05722b44473d318a0e167ebf4770d62e1a3770a3
Nathan Malkin <nmalkin@cs.brown.edu>
2011-05-07 18:02:21 -0400

    enforce file extensions on save/export. also, more abstract and flexible save code.

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -3,6 +3,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileFilter;
@@ -24,13 +25,15 @@ public class MainWindow extends JFrame {
 		
 		// state file chooser
 		final JFileChooser stateFileChooser = new JFileChooser();
+		FileFilter filter = new FileNameExtensionFilter("Domus XML file", "xml");
+		stateFileChooser.setFileFilter(filter);
 		stateFileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
 		
 		// html export file chooser
 		final JFileChooser exportFileChooser = new JFileChooser();
 		exportFileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-		FileFilter filter = new FileNameExtensionFilter("HTML file", "htm", "html");
-		exportFileChooser.addChoosableFileFilter(filter);
+		filter = new FileNameExtensionFilter("HTML file", "html");
+		exportFileChooser.setFileFilter(filter);
 		
 		// menu bar items
 		// load state button
@@ -72,25 +75,11 @@ public class MainWindow extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				try {
-					int returnVal = stateFileChooser.showSaveDialog(MainWindow.this);
-					if (returnVal == JFileChooser.APPROVE_OPTION) {
-						File file = stateFileChooser.getSelectedFile();
-						
-						if(file.exists()) {
-							int option = JOptionPane.showConfirmDialog(MainWindow.this, 
-									"This file already exists. Are you sure you want to overwrite it?", 
-									"Domus", 
-									JOptionPane.OK_CANCEL_OPTION);
-							
-							if(option == JOptionPane.OK_OPTION) {
-								DomusXML.writeXML(file);
-							}
-						} else {
-							DomusXML.writeXML(file);
-						}
+					File file = getValidSaveFile(stateFileChooser, "xml");
+					
+					if(file != null) {
+						DomusXML.writeXML(file);
 					}
-					
-					
 				} catch(java.io.IOException err) {
 					System.err.println("an error occurred writing to output");
 					//TODO: display a message
@@ -105,22 +94,10 @@ public class MainWindow extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				try {
-					int returnVal = exportFileChooser.showSaveDialog(MainWindow.this);
-					if (returnVal == JFileChooser.APPROVE_OPTION) {
-						File file = exportFileChooser.getSelectedFile();
-						
-						if(file.exists()) {
-							int option = JOptionPane.showConfirmDialog(MainWindow.this, 
-									"This file already exists. Are you sure you want to overwrite it?", 
-									"Domus", 
-									JOptionPane.OK_CANCEL_OPTION);
-							
-							if(option == JOptionPane.OK_OPTION) {
-								DomusXML.writeHTML(file);
-							}
-						} else {
-							DomusXML.writeHTML(file);
-						}
+					File file = getValidSaveFile(exportFileChooser, "html");
+					
+					if(file != null) {
+						DomusXML.writeHTML(file);
 					}
 				
 				} catch(java.io.IOException err) {
@@ -216,5 +193,69 @@ public class MainWindow extends JFrame {
 		showMessage(message, JOptionPane.PLAIN_MESSAGE);
 	}
 	
+	/**
+	 * Prompts the user for a file to save to.
+	 * If the selected file doesn't have the right extension, appends it automatically.
+	 * If the target file exists, has the user confirm their intent or try again if necessary.
+	 * 
+	 * TODO: accept multiple allowed extensions
+	 * 
+	 * @param fc the file chooser to open the dialog with
+	 * @param extension the allowed file extension
+	 * @return the File that the user wants to save to, or null if the user doesn't want to save (clicked cancel)
+	 */
+	private File getValidSaveFile(JFileChooser fc, String extension) {
+		File saveFile;
+		
+		boolean repeat;
+		do {
+			saveFile = null;
+			repeat = false;
+			
+			int returnVal = fc.showSaveDialog(MainWindow.this);
+			
+			if(returnVal == JFileChooser.APPROVE_OPTION) {
+				saveFile = fc.getSelectedFile();
+				
+				if(! extension.equals(getExtension(saveFile))) {
+					// append (a) valid file extension to get a new file
+					String newFile = saveFile.getAbsolutePath() + "." + extension;
+					saveFile = new File(newFile);
+				}
+				
+				if(saveFile.exists()) {
+					int option = JOptionPane.showConfirmDialog(MainWindow.this, 
+							"This file already exists. Are you sure you want to overwrite it?", 
+							"Domus", 
+							JOptionPane.OK_CANCEL_OPTION);
+					
+					if(option == JOptionPane.CANCEL_OPTION) {
+						repeat = true;
+					}
+				}
+			}
+		} while(repeat);
+		
+		return saveFile;
+	}
 	
+	/**
+	 * Given a file, returns its extension.
+	 * 
+	 * Code taken directly from:
+	 * http://download.oracle.com/javase/tutorial/uiswing/examples/components/FileChooserDemo2Project/src/components/Utils.java
+	 * 
+	 * @param f file whose extension we're looking for
+	 * @return the file extension, or null if there isn't any
+	 */
+	protected static String getExtension(File f) {
+        String ext = null;
+        String s = f.getName();
+        int i = s.lastIndexOf('.');
+
+        if (i > 0 &&  i < s.length() - 1) {
+            ext = s.substring(i+1).toLowerCase();
+        }
+        return ext;
+    }
 }
